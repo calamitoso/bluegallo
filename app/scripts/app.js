5f0aa96e03b2955d5e553c16982c9d7b85b155cc
Nicolas Herrera <calamitoso@gmail.com>
2016-03-23 06:48:38 -0400

    Basic project setup

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name bluegalloApp
+ * @description
+ * # bluegalloApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('bluegalloApp', [
+    'ngAnimate',
+    'ngRoute',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bluegalloApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bluegalloApp
+ */
+angular.module('bluegalloApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
